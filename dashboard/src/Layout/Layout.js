2a56bb671186ee175ad7bdd275bae56df82a75ce
sashka <dixxiesasha@gmail.com>
2018-01-03 00:25:00 +0500

    first commit

---
+++ dashboard/src/Layout/Layout.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Header from './Header/Header';
+import SubHeader from './Header/SubHeader';
+import Balance from './Widgets/Balance';
+import Rate from './Widgets/Rate';
+import Speed from './Widgets/Speed';
+import Downloads from './Widgets/Downloads';
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className='wrap'>
+        <Header />
+        <SubHeader />
+        <div className='widgets'>
+          <Balance />
+          <Rate />
+          <Speed />
+          <Downloads />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Layout;
