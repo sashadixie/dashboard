21d5c2d4c33f033cdfec80ce9d6ecf72404d6ced
sasha <dixxiesasha@gmail.com>
2018-01-04 22:56:42 +0500

    added history mode to make it possible to go back in history

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
-import Layout from './Layout/Layout';
-import Header from './Header/Header';
-import NotFound from './NotFound/NotFound';
-import Payment from './Payment/Payment';
-import Profile from './Profile/Profile';
-import Exit from './Exit/Exit';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Container from './Container';
+import { withRouter } from 'react-router'
+import { Router, Switch, Route, Link } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+const history = createBrowserHistory();
 class App extends Component {
+  constructor(props) {
+    super(props);
+    history.listen((location, action) => {
+      this.setState({
+        historyLink: location.pathname
+      });
+    });
+
+    this.state = {
+      historyLink: '/'
+    }
+  }
   render() {
+    const {historyLink} = this.state;
+
     return (
-      <Router>
-        <div>
-          <Header />
-          <Route exact path='/' component={Layout} />
-          <Route path='/payment' component={Payment} />
-          <Route path='/profile' component={Profile} />
-          <Route path='/exit' component={Exit} />
-        </div>
+      <Router history={history}>
+        <Container historyLink={historyLink} />
       </Router>
     );
   }
--- dashboard/src/Header/Header.js
+++ dashboard/src/Header/Header.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './Header.sass';
 
 class Header extends Component {
   render() {
+    const {onTabClick, links, link} = this.props;
+
     return (
       <header>
         <div className='topHeader'>
-          <div className='logo'>logo</div>
-          <div className='userInfo'>user pic and stuff</div>
+          <div className='logo'>
+            <img src='logo.png'/>
+          </div>
+          <div className='userInfo'>
+            <img src='userpic.png' />
+            <div className='userInfo__static'>
+              <div>Тестовый Пользователь Сын-Своего-Отца</div>
+              <div>Лицевой счёт № 1337228</div>
+            </div>
+            <i className='material-icons'>exit_to_app</i>
+          </div>
         </div>
-        <ul>
-          <li>
-            <Link exact to='/'>
-              Просмотр состояния
-            </Link>
-          </li>
-          <li>
-            <Link to='/payment'>
-              Способы оплаты
-            </Link>
-          </li>
-          <li>
-            <Link to='/profile'>
-              Настройки профиля
-            </Link>
-          </li>
-          <li>
-            <Link to='/exit'>
-              Выйти
-            </Link>
-          </li>
+        <ul className='sideBar'>
+        {
+          links.map(item => {
+            return (
+              <li className={`sideBar__link ${item.path === link ? 'active' : ''}`} data-link='/' onClick={onTabClick}>
+                <Link exact={item.exact} to={item.path} data-link={item.path}>
+                  {item.name}
+                </Link>
+              </li>
+            );
+          })
+        }
         </ul>
       </header>
     );
